ba6f3e02f78bbbb75159bd7ab122e11017509bdd
swapnilKaushik <swapnilkaushik07@gmail.com>
2020-09-03 18:20:55 +0530

    gallery

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,9 @@ import { HomeComponent } from './components/home/home.component'
 import { HeaderComponent } from './components/header/header.component'
 import { FooterComponent } from './components/footer/footer.component'
 import { AboutUsComponent } from './components/about-us/about-us.component';
+import { GalleryComponent } from './components/gallery/gallery.component';
+import { AlbumComponent } from './components/album/album.component';
+import { VideoGalleryComponent } from './components/video-gallery/video-gallery.component';
 
 
 const routes: Routes = [
@@ -11,13 +14,17 @@ const routes: Routes = [
 	{ path: 'home', component: HomeComponent },
 	{ path: 'header', component: HeaderComponent },
 	{ path: 'footer', component: FooterComponent },
-	{ path: 'about', component: AboutUsComponent }
+	{ path: 'about', component: AboutUsComponent },
+	{ path: 'gallery', component: GalleryComponent },
+	{ path: 'album', component: AlbumComponent },
+	{ path: 'album/:id', component: AlbumComponent },
+	{ path: 'video', component: VideoGalleryComponent },
 ];
 
 
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes)],  //, { useHash: true }
   exports: [RouterModule]
 })
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { RouterModule } from "@angular/router";
+import { NgxGalleryModule } from 'ngx-gallery-9';
+import { HttpClientModule } from '@angular/common/http';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { AboutUsComponent } from './components/about-us/about-us.component';
+import { GalleryComponent } from './components/gallery/gallery.component';
+import { AlbumComponent } from './components/album/album.component';
+import { VideoGalleryComponent } from './components/video-gallery/video-gallery.component';
+
+import { YouTubePlayerModule } from '@angular/youtube-player'
+
+/* Custom Hammer configuration */
+import { HammerGestureConfig, HAMMER_GESTURE_CONFIG } from '@angular/platform-browser';
+import * as Hammer from 'hammerjs';
+
+export class CustomHammerConfig extends HammerGestureConfig {
+  overrides = {
+    'pan': {
+      direction: Hammer.DIRECTION_ALL,
+    }
+  }
+}
+/* End Custom hammer configuration */
 
 @NgModule({
   declarations: [
@@ -14,13 +36,22 @@ import { AboutUsComponent } from './components/about-us/about-us.component';
     HomeComponent,
     HeaderComponent,
     FooterComponent,
-    AboutUsComponent
+    AboutUsComponent,
+    GalleryComponent,
+    AlbumComponent,
+    VideoGalleryComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgxGalleryModule,
+    HttpClientModule,
+    YouTubePlayerModule
   ],
-  providers: [],
+  providers: [
+    { provide: HAMMER_GESTURE_CONFIG, useClass: CustomHammerConfig }
+  ],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
